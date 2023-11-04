60096c4fba614d3b48d918c059402bcc360fa13d
Ajifirlana <firlana89@gmail.com>
2023-11-05 02:37:50 +0700

    Update berhasil_login.php

--- berhasil_login.php
+++ berhasil_login.php
@@ -19,11 +19,39 @@ if (!isset($_SESSION['username'])) {
 <body>
     <div class="container-logout">
         <form action="logout.php" method="POST" class="login-email">
-            <h1>Selamat Datang, <?php echo $_SESSION['username']; ?>!</h1>
+            <h1>Selamat Datang, <p><?php echo $_SESSION['username']; ?></p></h1>
             <div class="input-group">
                 <button type="submit" class="btn">Logout</button>
             </div>
         </form>
+        <table border="1">
+		<tr>
+			<th>NO</th>
+			<th>Nama</th>
+			<th>NIM</th>
+			<th>Alamat</th>
+			<th>OPSI</th>
+		</tr>
+		<?php 
+		include 'koneksi.php';
+		$no = 1;
+		$data = mysqli_query($conn,"select * from mahasiswa");
+		while($d = mysqli_fetch_array($data)){
+			?>
+			<tr>
+				<td><?php echo $no++; ?></td>
+				<td><?php echo $d['nama']; ?></td>
+				<td><?php echo $d['nim']; ?></td>
+				<td><?php echo $d['alamat']; ?></td>
+				<td>
+					<a href="edit.php?id=<?php echo $d['id']; ?>">EDIT</a>
+					<a href="hapus.php?id=<?php echo $d['id']; ?>">HAPUS</a>
+				</td>
+			</tr>
+			<?php 
+		}
+		?>
+	</table>
     </div>
 </body>
 </html>
